68fa141f2c7756c547c299249bef5750c5543060
Max <max.kuzn.msx@gmail.com>
2024-05-25 05:53:33 +0300

    add 403 interceptors

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+
 export const $host = axios.create({
     baseURL: 'http://localhost:3500',
     // withCredentials: false,
@@ -13,3 +14,13 @@ $host.interceptors.request.use((config) => {
     config.headers.Authorization = `Basic ${localStorage.getItem("token")}`
     return config
 })
+$host.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (error.response.status === 403) {
+        console.log("asd");
+
+        window.location.href = "/login";
+    }
+    return error;
+});
